43d2caa4e21ee043a0b101d3b27170ab9d7ba27d
Quinten <quinten.roets@gmail.com>
2021-12-27 03:15:19 +0100

    more precise timer

---
+++ libs/test/timer.py
@@ -0,0 +1,3 @@
+from libs import timer
+with timer.Timer():
+    import time; time.sleep(1.2) 
--- libs/timer.py
+++ libs/timer.py
@@ -1,14 +1,28 @@
-from datetime import datetime
+import time
+import datetime
 
 class Timer:
-    def __init__(self, message=None):
+    def __init__(self, message=""):
         self.message = message
+        if self.message and "{}" not in self.message:
+            self.message += ": "
 
     def __enter__(self, message=None):
-        self.start = datetime.now()
+        self.start = time.time()
 
     def __exit__(self, exception_type, exception_value, exception_traceback):
-        time = datetime.now() - self.start
-        time = str(time)[:-3]
-        message = self.message.format(time) if self.message else time
+        seconds = time.time() - self.start
+        if seconds > 1:
+            seconds = round(seconds, 3)
+        
+        interval = datetime.timedelta(seconds=seconds)
+        interval = str(interval)
+                    
+        #while interval[0] in "0:" and interval[1] != ".":
+        #interval = interval[1:]
+                    
+        while interval[-1] == "0":
+            interval = interval[:-1]
+        
+        message = self.message.format(interval) if "{}" in self.message else f"{self.message}{interval}"
         print(message)
