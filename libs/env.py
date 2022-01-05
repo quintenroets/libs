15d1aee632ab200301c2b3417a22de9297287b8f
Quinten <quinten.roets@gmail.com>
2022-01-05 23:48:35 +0100

    make path avaiable as argument

--- libs/env.py
+++ libs/env.py
@@ -2,6 +2,8 @@ from dotenv import load_dotenv
 from plib import Path
 
 
-def load():
-    load_dotenv(dotenv_path=Path.HOME / ".bash_profile")
+def load(path=None):
+    if path is None:
+        path = Path.HOME / ".bash_profile"
+    load_dotenv(dotenv_path=path)
     
