b594cfe4b4c58b72029bcacc63e379fc2ce483d2
Quinten <quinten.roets@gmail.com>
2022-01-06 13:53:12 +0100

    explain startfile function

--- libs/opener.py
+++ libs/opener.py
@@ -2,8 +2,10 @@ import os
 
 from libs.cli import Cli
 
-def startfile(file):
-    if os.path.islink(file):
-        file = readlink(file)
-        
-    Cli.run(f"xdg-open '{file}'", wait=False)
+
+def open_files(*filenames):
+    if os.name == 'nt':
+        for filename in filenames:
+            os.startfile(filename)
+    else:
+        Cli.run((f'xdg-open "{filename}"' for filename in filenames), wait=False)
