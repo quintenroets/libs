070bc6526a7fb0c3e99e2d61d0891f327ed5a705
Quinten <quinten.roets@gmail.com>
2022-01-15 12:38:43 +0100

    dont use timer

--- libs/test/pathtest.py
+++ libs/test/pathtest.py
@@ -1,9 +1,9 @@
 from plib import Path
-from libs.timer import Timer
 
-with Timer():
-    def match(path):
-        return (path / ".git").exists()
-    
-    for _ in Path.docs.find(match, only_folders=True):
-        pass 
+
+def match(path):
+    return (path / ".git").exists()
+
+
+for _ in Path.docs.find(match, only_folders=True):
+    pass
