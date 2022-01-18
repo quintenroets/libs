520c834d119ebce0b913ca9c57575fbd4fe858c4
Quinten <quinten.roets@gmail.com>
2022-01-18 17:16:52 +0100

    only show nanoseconds if no milliseconds and add option to change

--- libs/timer.py
+++ libs/timer.py
@@ -3,20 +3,19 @@ import datetime
 
 
 class Timer:
-    def __init__(self, message=None):
+    def __init__(self, message=None, full=False):
         self.message = message or '{}'
+        self.full = full
 
     def __enter__(self, message=None):
         self.start = time.time()
 
     def __exit__(self, exception_type, exception_value, exception_traceback):
         seconds = time.time() - self.start
-        if seconds > 1:
-            seconds = round(seconds, 3)
-        
-        interval = str(datetime.timedelta(seconds=seconds))
-        while interval[-1] == '0':
-            interval = interval[:-1]
+        interval_full = str(datetime.timedelta(seconds=seconds))
+        interval = interval_full[:-3]
+        if seconds < 1 / 1000 or self.full:  # only show nanoseconds when no milliseconds
+            interval += f"'{interval_full[-3:]}"
         
         message = self.message.format(interval) if '{}' in self.message else f'{self.message}: {interval}'
         print(message)
