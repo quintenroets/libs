ce7fc1ef282624b9852109edef4ccdb1529d00fa
Quinten <quinten.roets@gmail.com>
2022-01-24 13:30:34 +0100

    add silent option and total time counter

--- libs/timer.py
+++ libs/timer.py
@@ -3,19 +3,25 @@ import datetime
 
 
 class Timer:
-    def __init__(self, message=None, full=False):
+    total = 0
+
+    def __init__(self, message=None, full=False, silent=False):
         self.message = message or '{}'
         self.full = full
+        self.silent = silent
 
     def __enter__(self, message=None):
         self.start = time.time()
 
     def __exit__(self, exception_type, exception_value, exception_traceback):
         seconds = time.time() - self.start
-        interval_full = str(datetime.timedelta(seconds=seconds))
-        interval = interval_full[:-3]
-        if seconds < 1 / 1000 or self.full:  # only show nanoseconds when no milliseconds
-            interval += f"'{interval_full[-3:]}"
-        
-        message = self.message.format(interval) if '{}' in self.message else f'{self.message}: {interval}'
-        print(message)
+        Timer.total += seconds  # keep global counter as well
+
+        if not self.silent:
+            interval_full = str(datetime.timedelta(seconds=seconds))
+            interval = interval_full[:-3]
+            if seconds < 1 / 1000 or self.full:  # only show nanoseconds when no milliseconds
+                interval += f"'{interval_full[-3:]}"
+
+            message = self.message.format(interval) if '{}' in self.message else f'{self.message}: {interval}'
+            print(message)
