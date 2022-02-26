f80e96c06213822dafd34a0894521a8b72f883f2
Quinten <quinten.roets@gmail.com>
2022-02-26 15:21:52 +0100

    implement functionality to save locals vars from other function

--- libs/variables.py
+++ libs/variables.py
@@ -1,12 +1,27 @@
+import inspect
 import types
 
+from rich import pretty
 
-def save(**kwargs):
-    for k, v in kwargs.items():
+vars = {}
+
+
+def save_locals():
+    local_vars = inspect.stack()[1].frame.f_locals  # locals of one function above
+    local_vars = {k: v for k, v in local_vars.items() if not k.startswith("_")}
+    vars.update(local_vars)
+
+
+def get_locals():
+    return vars
+
+
+def insert_locals():
+    for k, v in get_locals().items():
         globals()[k] = v
 
 
-def show():
+def show_globals():
     exclude_types = [types.ModuleType, types.FunctionType]
     global_vars = {
         k: v
@@ -14,6 +29,5 @@ def show():
         if not k.startswith("_")
         and not (any([isinstance(v, e) for e in exclude_types]))
     }
-    message = "Variables: " + ", ".join(global_vars)
-    print(message)
-    return global_vars
+    pretty.pprint("Globals: ")
+    pretty.pprint(global_vars)
